f4b1b24802a9245e4018bdf529afbc9a8c00e35c
CodeAkio <siqueira_2004@hotmail.com>
2021-12-15 13:40:02 -0300

    Create logo atom

---
+++ components/atoms/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <c-box w="100%" class="logo">
+    <img src="@/assets/img/logo.svg" alt="Link Boss" />
+  </c-box>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { CBox } from '@chakra-ui/vue'
+
+export default Vue.extend({
+  name: 'Logo',
+  components: {
+    CBox,
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  img {
+    width: 100%;
+  }
+}
+</style>
