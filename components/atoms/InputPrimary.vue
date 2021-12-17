44cbed8dd18046594725953ddc01c57c7c337728
CodeAkio <siqueira_2004@hotmail.com>
2021-12-17 17:52:01 -0300

    Create input primary atom

---
+++ components/atoms/InputPrimary.vue
@@ -0,0 +1,18 @@
+<template>
+  <c-input
+    class="input-primary"
+    bg="background.500"
+    border-color="lightGrey.500"
+    focus-border-color="primary.500"
+    v-bind="[$props, $attrs]"
+    v-on="$listeners"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'InputPrimary',
+})
+</script>
