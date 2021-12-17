c7709c26695ab1c3775e5d3a9faffe6cf601b520
CodeAkio <siqueira_2004@hotmail.com>
2021-12-16 23:15:26 -0300

    create redirect page

---
+++ pages/sl/_shortCode.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  async asyncData({ params, error, $axios, redirect }) {
+    try {
+      const data = await $axios.$get(`/short-links/${params.shortCode}`)
+
+      redirect(data.original_url)
+      return {}
+    } catch (err) {
+      error({ statusCode: 404, message: 'URL não encontrada' })
+    }
+  },
+})
+</script>
