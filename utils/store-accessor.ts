9c3be02e59b3d39693e7021327b7681a8704b776
CodeAkio <siqueira_2004@hotmail.com>
2021-12-17 14:07:46 -0300

    Config vuex

---
+++ utils/store-accessor.ts
@@ -0,0 +1,7 @@
+import { Store } from 'vuex'
+// import { getModule } from 'vuex-module-decorators'
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const initializeStores = (store: Store<any>): void => {}
+
+export { initializeStores }
