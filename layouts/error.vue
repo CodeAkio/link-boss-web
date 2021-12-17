d3dc51177ac91438a387d9380f5f9615d433d3dd
CodeAkio <siqueira_2004@hotmail.com>
2021-12-16 22:06:05 -0300

    Create not found page

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,10 +1,20 @@
 <template>
-  <h1>404</h1>
+  <c-box class="container">
+    <c-flex w="100vw" h="100vh" bg="background.500" flex-dir="column">
+      <Header />
+      <ContentCentered>
+        <img
+          class="not-found-image"
+          src="@/assets/img/404.svg"
+          alt="Não encontrado"
+        />
+      </ContentCentered>
+    </c-flex>
+  </c-box>
 </template>
 
-<script>
-export default {
-  layout: 'blank',
+<style lang="scss" scoped>
+.not-found-image {
+  max-width: 50%;
 }
-</script>
-
+</style>
